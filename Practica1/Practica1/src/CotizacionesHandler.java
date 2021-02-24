2b7eb3f80911ef4a00ec2acee71e27e559f27456
Pedro Allué <pedro.allue.tamargo@gmail.com>
2021-02-24 17:01:05 +0100

    Intentamos jugar con el XML y EFE pero tiene buena pinta

--- Practica1/Practica1/src/CotizacionesHandler.java
+++ Practica1/Practica1/src/CotizacionesHandler.java
@@ -7,9 +7,18 @@ public class CotizacionesHandler extends DefaultHandler {
     private Hashtable<String, Double> tabla = new Hashtable<String, Double>();
 
     // TODO: Añadir más cosas
+    private String empresa;
+    private boolean leeEmpresa;
+
+    private Double cotizacion;
+    private boolean leeCotizacion;
 
     @Override
     public void startDocument() throws SAXException {
+        empresa = "";
+        leeEmpresa = false;
+        cotizacion = 0.0;
+        leeCotizacion = false;
     }
 
     @Override
@@ -18,14 +27,38 @@ public class CotizacionesHandler extends DefaultHandler {
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        System.out.println("Comienza el elmento: <" + qName + ">");
+        if (qName.equals("lista_cotizaciones")) {
+            // Es un conjunto de empresas --> No hacemos nada
+            leeEmpresa = false;
+            leeCotizacion = false;
+        } else if (qName.equals("empresa")) {
+            // Es una empresa --> Guardar su valor en la variable temporal
+            leeEmpresa = true;
+            leeCotizacion = false;
+        } else if (qName.equals("cotizaciones")) {
+            // Es una cotizacion --> Asociar a la empresa que sea
+            leeEmpresa = false;
+            leeCotizacion = true;
+        }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
+        // Nada
+        System.out.println("Fin del elmento: <" + qName + ">");
     }
 
     @Override
     public void characters(char ch[], int start, int length) throws SAXException {
+//        System.out.println("Texto de dentro: " + new String(ch));
+        if (leeEmpresa) {
+            empresa = new String(ch);
+        } else if (leeCotizacion) {
+            String tmp = new String(ch);
+            cotizacion = Double.parseDouble(tmp);
+            tabla.put(empresa, cotizacion);
+        }
     }
 
     public Hashtable<String, Double> getTabla() {
