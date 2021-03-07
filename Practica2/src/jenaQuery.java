5443b29c85d0371983bdbdc0647a9b1b7d712acc
jtambo99 <jtambo@live.com>
2021-03-07 23:42:51 +0100

    Redirecciones funcionan.

--- Practica2/src/jenaQuery.java
+++ Practica2/src/jenaQuery.java
@@ -1,6 +1,5 @@
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.RDFNode;
 import twitter4j.*;
 
 import java.util.*;
@@ -13,28 +12,16 @@ public class jenaQuery {
 //                "Torres", "Ronaldo", "Pablo Iglesias", ""};
 
         // Dynamic version
-        List<String> tendencias = getTwitterTrends();
+        List<String> tendencias = new ArrayList<>();//getTwitterTrends();
 
         Set<String> resultados = new HashSet<String>();
-        String sparqlEndpoint = "http://dbpedia-live.openlinksw.com/sparql";
+        String sparqlEndpointLive = "http://dbpedia-live.openlinksw.com/sparql";
+        String sparqlEndpoint = "https://dbpedia.org/sparql";
         tendencias.add("Neymar_Jr");
         tendencias.add("Neymar");
+
         for (String tendencia : tendencias) {
-//            String selectQuery = ...
-//            org.apache.jena.query.Query query = QueryFactory.create(selectQuery);
-//            QueryExecution exec = QueryExecutionFactory.sparqlService(
-//                    sparqlEndpoint, query);
-//            try {
-//                ResultSet results = exec.execSelect();
-//                while (results.hasNext()) {
-//                    QuerySolution qs = results.nextSolution();
-//                    RDFNode n = qs.get(variableName);
-//                    if (!n.isLiteral())
-//                        resultados.add(n.toString());
-//                }
-//            } finally {
-//                exec.close();
-//            }
+
             // RDF endpoints sobre los que realizar la query
             // Se han añadido varios puntos de búsqueda. Para realizar la consulta se deberá hacer UNION
             // Dentro de rdf:type:
@@ -46,8 +33,10 @@ public class jenaQuery {
             // Dentro de dbp:wikiPageUsesTemplate:
             String wikiEndpoints[] = {"dbt:FIFA_player", "dbt:UEFA_player"};
 
-//            String tendenciaWithSpaces = tendencia.replace("_", " ");
-            String tendenciaWithSpaces = tendencia;
+            String tendenciaWithoutSpaces = tendencia.replace(" ", "_");
+
+            System.out.println("Buscando: " + tendenciaWithoutSpaces + " ...");
+
             String askQuery = String.format("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
                             "PREFIX yago: <http://dbpedia.org/class/yago/> " +
                             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
@@ -57,33 +46,66 @@ public class jenaQuery {
                             "PREFIX dbt: <http://dbpedia.org/resource/Template:>" +
                             "PREFIX dbo: <http://live.dbpedia.org/ontology/wikiPageRedirects>" +
                             "PREFIX dbr: <http://dbpedia.org/resource/>" +
-                            "ASK WHERE {" +
-                            "{ ?x rdf:type yago:FootballPlayer110101634 . ?x rdfs:label \"%s\"@en}" +
+                            "ASK {" +
+                            "{ dbr:%s rdf:type yago:FootballPlayer110101634}" +
                             "UNION" +
-                            "{ ?x rdf:type yago:LaLigaFootballers . ?x rdfs:label \"%s\"@en}" +
+                            "{ dbr:%s rdf:type yago:LaLigaFootballers}" +
                             "UNION" +
-                            "{ ?x dct:subject dbc:LaLigaFootballers . ?x rdfs:label \"%s\"@en}" +
+                            "{ dbr:%s dct:subject dbc:LaLigaFootballers}" +
                             "UNION" +
-                            "{ ?x dct:subject dbc:Spanish_footballers . ?x rdfs:label \"%s\"@en}" +
+                            "{ dbr:%s dct:subject dbc:Spanish_footballers}" +
                             "UNION" +
-                            "{ ?x dct:subject dbc:Spain_international_footballers . ?x rdfs:label \"%s\"@en}" +
+                            "{ dbr:%s dct:subject dbc:Spain_international_footballers}" +
                             "UNION" +
-                            "{ ?x dbp:wikiPageUsesTemplate dbt:FIFA_player . ?x rdfs:label \"%s\"@en}" +
+                            "{ dbr:%s dbp:wikiPageUsesTemplate dbt:FIFA_player}" +
                             "UNION" +
-                            "{ ?x dbp:wikiPageUsesTemplate dbt:UEFA_player . ?x rdfs:label \"%s\"@en}" +
-                            "UNION" +
-                            "{ ?x dbo:wikiPageRedirects ?y . ?y rdf:type yago:FootballPlayer110101634 .  FILTER (SAMETERM(?x, \"%s\"))}}"
-                    , tendenciaWithSpaces, tendenciaWithSpaces,
-                    tendenciaWithSpaces, tendenciaWithSpaces,
-                    tendenciaWithSpaces, tendenciaWithSpaces,
-                    tendenciaWithSpaces, tendenciaWithSpaces, tendenciaWithSpaces);
+                            "{ dbr:%s dbp:wikiPageUsesTemplate dbt:UEFA_player}}"
+                    , tendenciaWithoutSpaces, tendenciaWithoutSpaces,
+                    tendenciaWithoutSpaces, tendenciaWithoutSpaces,
+                    tendenciaWithoutSpaces, tendenciaWithoutSpaces,
+                    tendenciaWithoutSpaces, tendenciaWithoutSpaces);
 
             org.apache.jena.query.Query query = QueryFactory.create(askQuery);
 
             QueryExecution exec = QueryExecutionFactory.sparqlService(
-                    sparqlEndpoint, query);
-            if (exec.execAsk())
+                    sparqlEndpointLive, query);
+            if (exec.execAsk()) {
                 resultados.add(tendencia);
+            }
+
+            // Esta consulta se hace en caso de no sea futbolista con la consulta ASK anteriror
+            // Se hace sobre la dbpedia original ya que se han tenido problemas de busqueda
+            String selectQuery = String.format("PREFIX dbo: <http://dbpedia.org/ontology/>" +
+                    "PREFIX dbr: <http://dbpedia.org/resource/>" +
+                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                    "PREFIX yago: <http://dbpedia.org/class/yago/>" +
+                    "PREFIX umbel-rc: <http://umbel.org/umbel/rc/>" +
+                    "SELECT DISTINCT ?redirects " +
+                    "WHERE " +
+                    "{ {dbr:%s dbo:wikiPageRedirects ?redirects . ?redirects rdf:type dbo:SoccerPlayer}" +
+                    "UNION" +
+                    "{ dbr:%s dbo:wikiPageRedirects ?redirects . ?redirects rdf:type yago:FootballPlayer110101634}" +
+                    "UNION" +
+                    "{ dbr:%s dbo:wikiPageRedirects ?redirects . ?redirects rdf:type umbel-rc:SoccerPlayer}}",
+                    tendenciaWithoutSpaces, tendenciaWithoutSpaces,tendenciaWithoutSpaces);
+
+            query = QueryFactory.create(selectQuery);
+            exec = QueryExecutionFactory.sparqlService(
+                    sparqlEndpoint, query);
+            try {
+                ResultSet results = exec.execSelect();
+                while (results.hasNext()) {
+                    QuerySolution qs = results.nextSolution();
+                    RDFNode n = qs.get("?redirects");
+                    System.out.println("Obtenido: " + n.toString());
+                    if (!n.isLiteral()){
+                        resultados.add(tendencia);
+                    }
+                }
+            } finally {
+                exec.close();
+            }
         }
 
         mostrarResultados(tendencias, resultados);
@@ -105,8 +127,8 @@ public class jenaQuery {
 
         // Recorremos las tendencias obtenidas
         for (Trend tren : dailyTrends.getTrends()) {
-//            System.out.println(tren.getName());
-//            System.out.println("*".repeat(30));
+            System.out.println(tren.getName());
+            System.out.println("*".repeat(30));
             // Se eliminan los posibles '#'
             list.add(tren.getName().replace("#", ""));
         }
