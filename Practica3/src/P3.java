b8307f30ea8fe1deda50c82f6e6767aec1d45dc2
Pedro Allué <pedro.allue.tamargo@gmail.com>
2021-03-11 18:50:14 +0100

    Somos dioses

--- Practica3/src/P3.java
+++ Practica3/src/P3.java
@@ -1,6 +1,5 @@
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.io.StreamDocumentTarget;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
@@ -12,6 +11,7 @@ public class P3 {
     private static OWLOntology ont;
     private static IRI ontologyIRI;
     private static OWLOntologyManager manager;
+    private static OWLReasoner reasoner;
 
     public static void main(String[] args) {
         try {
@@ -29,18 +29,83 @@ public class P3 {
             addMafioso("Bartomeu");
 
             // Crear el razonador
-            OWLReasoner reasoner = new Reasoner.ReasonerFactory().createReasoner(ont);
+            reasoner = new Reasoner.ReasonerFactory().createReasoner(ont);
             reasoner.precomputeInferences();
             // Ejemplo de consulta: consistencia
             System.out.println(reasoner.isConsistent());
             // Lanzamos consultas de la práctica
-            // ...
+            createSeparatorWithText("Mostrando individuos de la clase: Hombre");
+            showAllByClassName("Hombre");
+
+            createSeparatorWithText("Mostrando individuos de la clase: Mafioso");
+            showAllByClassName("Mafioso");
+
+            createSeparatorWithText("Mostrando individuos que son la esposa de Vito");
+            printEsposaVito();
+
+            createSeparatorWithText("Mostrando individuos que son descendientes de Vito");
+            printDescendientesVito();
+
+            createSeparatorWithText("Mostrando individuos que es padre de Michael");
+            printPadreMichael();
+
+
             // Salvamos la ontología
             manager.saveOntology(ont, IRI.create(fileDest.toURI()));
-            System.out.println("Ontología salvada correctamente");
+            createSeparatorWithText("Ontología salvada correctamente");
         } catch (Exception e) {
             System.err.println("Exception: " + e);
         }
+    }
+
+    private static void printEsposaVito() {
+        // Funcionaría también con cónyugeDe
+        // Source: https://stackoverflow.com/questions/27695956/retrieve-owl-individuals-with-same-object-properties-using-owl-api-4-0
+        OWLObjectProperty property = factory.getOWLObjectProperty(IRI.create(ontologyIRI + "esposaDe"));
+        OWLIndividual vito = factory.getOWLNamedIndividual(IRI.create(ontologyIRI + "Vito"));
+        OWLClassExpression classExpression = factory.getOWLObjectHasValue(property, vito.asOWLNamedIndividual());
+
+        for (OWLNamedIndividual individual : reasoner.getInstances(classExpression, false).getFlattened()) {
+            System.out.println(individual.getIRI());
+        }
+    }
+
+    private static void printDescendientesVito() {
+        OWLObjectProperty property = factory.getOWLObjectProperty(IRI.create(ontologyIRI + "descendienteDe"));
+        OWLIndividual vito = factory.getOWLNamedIndividual(IRI.create(ontologyIRI + "Vito"));
+        OWLClassExpression classExpression = factory.getOWLObjectHasValue(property, vito.asOWLNamedIndividual());
+
+        for (OWLNamedIndividual individual : reasoner.getInstances(classExpression, false).getFlattened()) {
+            System.out.println(individual.getIRI());
+        }
+    }
+
+    private static void printPadreMichael() {
+        OWLObjectProperty esHijoDe = factory.getOWLObjectProperty(IRI.create(ontologyIRI + "hijoDe"));
+        OWLClass esPadre = factory.getOWLClass(IRI.create(ontologyIRI + "Hombre"));
+        OWLIndividual michael = factory.getOWLNamedIndividual(IRI.create(ontologyIRI + "Michael"));
+        // Usamos la propiedad inversa de hijoDe -> salen todos los progenitores
+        OWLClassExpression hijoDe = factory.getOWLObjectHasValue(esHijoDe.getInverseProperty(), michael.asOWLNamedIndividual());
+        // Calculamos la interseccion
+        OWLClassExpression intersection = factory.getOWLObjectIntersectionOf(hijoDe, esPadre);
+
+        for (OWLNamedIndividual individual : reasoner.getInstances(intersection, false).getFlattened()) {
+            System.out.println(individual.getIRI());
+        }
+    }
+
+    private static void createSeparatorWithText(String message) {
+        System.out.println("*".repeat(24));
+        System.out.println(message);
+        System.out.println("*".repeat(24));
+    }
+
+    private static void showAllByClassName(String classname) {
+        OWLClass tClass = factory.getOWLClass(IRI.create((ontologyIRI + classname)));
+        for (OWLNamedIndividual instance : reasoner.getInstances(tClass, false).getFlattened()) {
+            System.out.println(instance.getIRI());
+        }
+
     }
 
     private static void addMafioso(String mafioso) throws OWLOntologyStorageException {
