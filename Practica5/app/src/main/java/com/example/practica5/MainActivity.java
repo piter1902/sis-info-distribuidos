b6f4893475af4cb7bff7c63cf803d9923a0c39af
jtambo99 <jtambo@live.com>
2021-04-12 13:35:42 +0200

    Comienzo de P5. Falta sincronizar Main con AsyncTask

--- Practica5/app/src/main/java/com/example/practica5/MainActivity.java
+++ Practica5/app/src/main/java/com/example/practica5/MainActivity.java
@@ -1,19 +1,46 @@
 package com.example.practica5;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    // Creamos cliente de google
+    private FusedLocationProviderClient fusedLocationClient;
+    private GoogleApiClient googleApiClient;
+    private MyAsyncTask myTask;
+    private Double latit;
+    private Double longi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +53,104 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                LocationRequest locationRequest = new LocationRequest();
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                locationRequest.setNumUpdates(1);
+                locationRequest.setInterval(0);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                fusedLocationClient.requestLocationUpdates(locationRequest,
+                        new LocationCallback() {
+                            @Override
+                            public void onLocationResult(LocationResult result) {
+                                if (result == null)
+                                    return;
+                                int lastIndex = result.getLocations().size() - 1;
+
+                                Location location = result.getLocations().get(lastIndex);
+
+                                // Actualizamos los valores de lat y lon
+                                latit = location.getLatitude();
+                                longi = location.getLongitude();
+                
+                                // Una vez obtenida la location, llamamos a asynctask para que realice la peticion a flickr
+                                myTask = (MyAsyncTask) getLastCustomNonConfigurationInstance();
+
+                                if (myTask == null) {
+                                    // Evita crear una AsyncTask cada vez que, por ejemplo, hay una rotación
+
+                                    myTask = new MyAsyncTask(MainActivity.this);
+
+                                    // Creamos la uri para realizar la petición
+                                    Uri queryFlickr = Uri.parse("https://api.flickr.com/services/rest/")
+                                            .buildUpon()
+                                            .appendQueryParameter("api_key", "e45df953d132e8de1fac638da4ed55bc")
+                                            .appendQueryParameter("method", "flickr.photos.search")
+                                            .appendQueryParameter("lat", latit.toString())
+                                            .appendQueryParameter("lon", longi.toString())
+                                            .appendQueryParameter("format", "json")
+                                            .appendQueryParameter("extras", "url_s")
+                                            .build();
+                                    Log.i("MainActivity", "Query: " + queryFlickr.toString());
+                                    // Hacemos la petición a la API
+                                    myTask.execute(queryFlickr.toString());
+                                } else {
+                                    myTask.attach(MainActivity.this);
+                                }
+                            }
+                        },
+                        Looper.getMainLooper()
+                );
             }
         });
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this,
+                new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            latit = location.getLatitude();
+                            longi = location.getLongitude();
+                        }
+                    }
+                }
+        );
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(LocationServices.API)
+                .build();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
     }
 
     @Override
@@ -52,5 +173,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
     }
 }
---
+++ Practica5/app/src/main/java/com/example/practica5/MyAsyncTask.java
@@ -0,0 +1,88 @@
+package com.example.practica5;
+
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+public class MyAsyncTask extends AsyncTask<String, Void, Integer> {
+
+    private MainActivity mActivity = null;
+
+    public MyAsyncTask(MainActivity activity) {
+        attach(activity);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected Integer doInBackground(String... params) {
+        HttpURLConnection connection;
+
+        try {
+            connection = (HttpURLConnection) new URL(params[0]).openConnection();
+            // Response from URL
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+//                Log.d("MyAsyncTask", connection.getContent().toString());
+                StringBuilder textBuilder = new StringBuilder();
+                try (Reader reader = new InputStreamReader
+                        (connection.getInputStream(), StandardCharsets.UTF_8)) {
+                    int c = 0;
+                    while ((c = reader.read()) != -1) {
+                        textBuilder.append((char) c);
+                    }
+                }
+                String result = textBuilder.toString();
+                result = result.replace("jsonFlickrApi(", "");
+                result = result.substring(0, result.length() - 1);
+//                Log.d("MyAsyncTask", result);
+                Gson gson = new Gson();
+//                Map map = gson.fromJson(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8), Map.class);
+                Map<String, Object> map = gson.fromJson(result, Map.class);
+//                Log.d("MyAsyncTask", "" + map.entrySet().size());
+                // Get photo object from photos entry
+                List<Map<String, Object>> photoList =
+                        ((List<Map<String, Object>>) ((Map) map.get("photos")).get("photo"));
+                for (Map<String, Object> stringObjectMap : photoList) {
+                    Log.d("MyAsyncTask", ((String) stringObjectMap.get("id")));
+                }
+                // Exclusion mutua
+                synchronized (mActivity.getPublications()) {
+                    mActivity.setPublications(photoList);
+                }
+            }
+            return responseCode;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        if (mActivity == null)
+            Log.i("MyAsyncTask", "Me salto onPostExecute() -- no hay nueva activity");
+        else
+            mActivity.setupAdapter(integer);
+    }
+
+    void detach() {
+        this.mActivity = null;
+    }
+
+    void attach(MainActivity activity) {
+        this.mActivity = activity;
+    }
+}
